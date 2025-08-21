b46bf4633e697adddc20646a6db0284c30428eac
Aditya Eka Pranata <aepranata@gmail.com>
2025-08-21 13:54:33 +0700

    version: Add maintainer flag
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -10,7 +10,8 @@ ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.lineage.releasetype=$(LINEAGE_BUILDTYPE) \
     ro.lineage.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
     ro.modversion=$(LINEAGE_VERSION) \
-    ro.lineagelegal.url=https://lineageos.org/legal
+    ro.lineagelegal.url=https://lineageos.org/legal \
+    ro.lineage.maintainer=$(LINEAGE_MAINTAINER)
 
 # LineageOS Platform Display Version
 ADDITIONAL_SYSTEM_PROPERTIES += \
